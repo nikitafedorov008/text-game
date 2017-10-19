c650472175e2307fe29a57c3699f11da412ee828
Alexey Leshchuk <kuelye@gmail.com>
2017-10-19 16:27:49 +0300

    add game base

--- src/com/banana/textgame/Main.java
+++ src/com/banana/textgame/Main.java
@@ -2,8 +2,45 @@ package com.banana.textgame;
 
 public class Main {
 
+    /*
+     * Главный метод.
+     */
     public static void main(String[] args) {
-	// write your code here
+        // вызывает метод start()
+        new Main().start();
+    }
+
+    /*
+     * Метод с логикой игры.
+     */
+    private void start() {
+        onStart();
+        for (int i = 1; i <= 5; ++i) {
+            onNewDay(i);
+        }
+        onFinish();
+    }
+
+    /*
+     * Метод вызывается один раз при старте игры.
+     */
+    void onStart() {
+
+    }
+
+    /*
+     * Метод вызывается каждый игровый день.
+     * Единственный параметр: dayNumber - номер текущего игрового дня.
+     */
+    void onNewDay(int dayNumber) {
+
+    }
+
+    /*
+     * Метод вызывается по завершению игры.
+     */
+    void onFinish() {
+
     }
 
 }
