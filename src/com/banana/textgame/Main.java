ca34d764fa46d7357a7653025ff291a8fea59b3b
Banana Android Z <ndrdbnnz@gmail.com>
2017-10-23 18:59:09 +0300

    Простая программа, Отвечает на 2 дейстаия

--- src/com/banana/textgame/Main.java
+++ src/com/banana/textgame/Main.java
@@ -1,5 +1,7 @@
 package com.banana.textgame;
 
+import java.util.Scanner;
+
 public class Main {
 
     /*
@@ -25,15 +27,31 @@ public class Main {
      * Метод вызывается один раз при старте игры.
      */
     void onStart() {
-
+        Scanner keyboard = new Scanner(System.in);
+        int a = 42, b = 27;
+        String строка = "Ответ на самый главный вопрос:Сила в ньютонах брат";
+        System.out.println("Здраствуй, как тебя зовут?");
+        String answer = keyboard.nextLine();
+        System.out.println("Здраствуй " + answer);
     }
 
+    int dollars = 0;
+
     /*
-     * Каждую игровую ночь ничего не происходит
+     * Каждую игровую ночь ничего не происходит!
      * Метод вызывается каждый игровый день.
      * Единственный параметр: dayNumber - номер текущего игрового дня.
      */
     void onNewDay(int dayNumber) {
+        Scanner keyboard = new Scanner(System.in);
+        int a = 42, b = 27;
+        String строка = "Ответ на самый главный вопрос:Сила в ньютонах брат";
+        System.out.println("Здраствуй, как тебя зовут?");
+        String код = keyboard.nextLine();
+        System.out.println("Здраствуй " + код);
+        dollars = dollars + код.length();
+
+        System.out.println("Твоя наличка: " + dollars);
 
     }
 
