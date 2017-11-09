aed5001b425ab4fe9b73459b603d51d0121b398c
Banana Android Z <ndrdbnnz@gmail.com>
2017-11-09 18:51:35 +0300

    игра в программиста 3

--- src/com/banana/textgame/Main.java
+++ src/com/banana/textgame/Main.java
@@ -29,13 +29,19 @@ public class Main {
     void onStart() {
         Scanner keyboard = new Scanner(System.in);
         int a = 42, b = 27;
-        String строка = "Ответ на самый главный вопрос:Сила в ньютонах брат";
-        System.out.println("Здраствуй, как тебя зовут?");
+        String строка = "Ответ на самый главный вопрос:Сила в ньютонах брат!";
+        String[] vs = {"Кто вы такой?", "Здраcтвуй товарищ!, представся.", "ЫЫЫыыыЫЫЫ"};
+        String v = vs[(int) (Math.random()* vs. length)];
+        System.out.println(v);
         String answer = keyboard.nextLine();
         System.out.println("Здраствуй " + answer);
     }
 
+    boolean trired = false;
     int dollars = 0;
+    String[] languages = {"java","Python","JavaScript","C++","Brainfuck"};
+    boolean[] knownLanguages = {true, false, false, false, false};
+    Scanner keyboard = new Scanner(System.in);
 
     /*
      * Каждую игровую ночь ничего не происходит!
@@ -49,11 +55,14 @@ public class Main {
             dollarsString = dollarsString + "";
         }
         System.out.println("Ваш счёт: " + dollarsString + ".");
+        напечатайИзвестныеЯзыки();
 
         Scanner клавиатура = new Scanner(System.in);
         System.out.println("Введите :");
         String action = клавиатура.nextLine();
         switch (action.toLowerCase()) {
+            case "изучить":
+                LearnLanguage();
             case "кофе":
                 dollars -= 2;
                 System.out.println("Кофе,ура!");
@@ -65,6 +74,9 @@ public class Main {
                 int заработок = пароль.length();
                 dollars = dollars + заработок;
                 System.out.println("Всего ты заработал: " + dollars + "$.");
+                break;
+            case "устал":
+                trired = true;
                 break;
 
             default:System.out.println("операция не поддерживаеться.");
@@ -92,7 +104,31 @@ public class Main {
      * Метод вызывается по завершению игры.
      */
     void onFinish() {
+        System.out.print("Приехали, ты заработал:" + dollars + "$");}
 
+        //действия
+
+    void LearnLanguage() {
+            System.out.println("Какой язык будем учить?");
+            String выбранныйязык = keyboard.nextLine();
+
+            for (int i = 0; i < languages.length; ++i) {
+                if (languages[i].equals(выбранныйязык)) {
+                    knownLanguages[i] = true;
+                    dollars -= 20;
+                }
+            }
     }
+
+    void напечатайИзвестныеЯзыки() {
+                System.out.println("Вы знаете: ");
+        for (int i = 0; i < languages.length; ++i) {
+            if (knownLanguages[i] == true) {
+                System.out.println(languages[i]);
+            }
+        }
+    }
+
+
 
 }
